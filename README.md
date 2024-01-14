5e84829768fda4affaa373a7b38ce27239ebe278
Tritus <jacktritus@gmail.com>
2024-01-14 17:24:02 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# swatch.js
+
+A library to overcomplicate the Swatch clock.
+
+Inspired by MelonLand's [Swatch Internet Time](https://wiki.melonland.net/swatch_time).
+
+### Installation
+
+#### Import on the page
+```html
+<script type="text/javascript" src="./swatch.js">
+```
+
+### Basic usage
+
+`Swatch(selector, options)`
+
+#### HTML
+```html
+<span class="mySwatch"></span>
+```
+#### Javascript
+```javascript
+Swatch('.mySwatch')
+```
+
+### Other options
+```javascript
+const mySwatch = Swatch('.mySwatch', {
+    prefix: '@', // Text to place before the clock
+    millis: 864, // Update every 864 milliseconds
+    realTime: true // Won't update if false
+})
+
+mySwatch.stop() // Stop updating
+mySwatch.start() // Resume updating
+```
---
+++ swatch.js
@@ -0,0 +1,63 @@
+(function() {
+    const defaults = {
+        prefix: '@',
+        millis: 864,
+        realTime: true
+    }
+    function getSwatchTime(showDecimals = true) {
+        const date = new Date()
+        const hh = date.getUTCHours()
+        const mm = date.getUTCMinutes()
+        const ss = date.getUTCSeconds()
+        const millis = date.getUTCMilliseconds()
+        const timeInMillis = ((hh * 60 + mm) * 60 + ss) * 1000 + millis
+        const swatchTime = Math.abs(timeInMillis / 86400)
+        return showDecimals ? swatchTime.toFixed(2) : Math.floor(swatchTime)
+    }
+    var Swatch = (obj, options) => {
+        const instance = {}
+        options = {...defaults, ...options}
+        instance.refreshSwatchClock = function() {
+            instance.element.innerText = (instance.options.prefix) + getSwatchTime()
+        }
+        instance.renderSwatchClock = function() {
+            const interval = setInterval(instance.refreshSwatchClock, instance.options.millis)
+            instance.refreshSwatchClock()
+            return interval
+        }
+        instance.start = function() {
+            if (instance.refreshInterval) {
+                console.info('Already started.')
+            } else {
+                instance.refreshInterval = instance.renderSwatchClock()
+            }
+        }
+        instance.stop = function() {
+            if (instance.refreshInterval) {
+                clearInterval(instance.refreshInterval)
+                instance.refreshInterval = null
+            } else {
+                console.info('Already stopped.')
+            }
+        }
+        if (typeof obj === 'string') {
+            // Selector
+            obj = document.querySelector(obj)
+        }
+        instance.element = obj
+        instance.options = options
+        if (instance.options.realTime) {
+            instance.start()
+        } else {
+            instance.refreshSwatchClock()
+        }
+        return instance
+    }
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Swatch
+    } else if (typeof define === 'function' && define.amd) {
+        define(Swatch)
+    } else if (window) {
+        window.Swatch = Swatch
+    }
+}());
